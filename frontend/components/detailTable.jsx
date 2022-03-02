dcbe63aae0310723be721c8805d89170f258d6e2
Andrew Vitovitch <alvitovitch@gmail.com>
2022-03-02 13:59:21 -0500

    parsed tables, removed excess divs

--- frontend/components/detailTable.jsx
+++ frontend/components/detailTable.jsx
@@ -5,72 +5,51 @@ export default function DetailTable(props){
     if (coins.length > 0) {
         return (
             
-                            <table className="w-2 divide-y divide-gray-200 overflow-y-scroll border-solid border 2 border-grey-500">
+                            <table className="w-2 divide-y divide-gray-200 overflow-y-scroll border-solid border 2 border-grey-500 table">
                                 <thead className="bg-gray-50">
                                     <tr>
-                                        <th scope="col" className="px-3 py-3 text-center text-sm font-medium text-gray-500 uppercase ">Rank</th>
-                                        <th scope="col" className="px-3 py-3 text-center text-sm font-medium text-gray-500 uppercase ">Id</th>
-                                        <th scope="col" className="px-3 py-3 text-center text-sm font-medium text-gray-500 uppercase ">Symbol</th>
-                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm font-medium text-gray-500 uppercase ">Image</th>
-                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm font-medium text-gray-500 uppercase ">Name</th>
-                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm font-medium text-gray-500 uppercase ">Market Cap</th>
-                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm font-medium text-gray-500 uppercase ">Current Price</th>
-                                        <th scope="col" className="px-3 py-3 text-center text-sm font-medium text-gray-500 uppercase ">ATL</th>
-                                        <th scope="col" className="px-3 py-3 text-center text-sm font-medium text-gray-500 uppercase ">ATH</th>
+                                        <th scope="col" className="px-3 py-3 text-center text-sm text-gray-500 uppercase ">Rank</th>
+                                        <th scope="col" className="px-3 py-3 text-center text-sm text-gray-500 uppercase ">Id</th>
+                                        <th scope="col" className="px-3 py-3 text-center text-sm text-gray-500 uppercase ">Symbol</th>
+                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm text-gray-500 uppercase ">Image</th>
+                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm text-gray-500 uppercase ">Name</th>
+                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm text-gray-500 uppercase ">Market Cap</th>
+                                        <th scope="col" className="px-3 py-3 whitespace-nowrap text-center text-sm text-gray-500 uppercase ">Current Price</th>
+                                        <th scope="col" className="px-3 py-3 text-center text-sm text-gray-500 uppercase ">ATL</th>
+                                        <th scope="col" className="px-3 py-3 text-center text-sm text-gray-500 uppercase ">ATH</th>
                                     </tr>
                                 </thead>
                                 <tbody className="bg-white  divide-y divide-gray-200 h-100">
                                     {coins.map(coin => (
                                         <tr key={coin.id}>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.market_cap_rank}</div>
-                                                    </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                    {coin.market_cap_rank}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.id}</div>
-                                                    </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                    {coin.id}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="flex text-xs justify-self-center font-medium text-gray-900">{coin.symbol}</div>
-                                                </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                    {coin.symbol}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
                                                     <img className="h-10" src={coin.image}></img>
-                                                </div>
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.name}</div>
-                                                </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                    {coin.name}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.market_cap}</div>
-                                                </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                   {coin.market_cap}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    {coin.current_price/coin.ath > 0.5 ? 
-                                                    <div className="text-xs font-medium text-blue-400">{coin.current_price}</div>
+                                                {coin.current_price/coin.ath > 0.5 ? 
+                                                    <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-blue-400">{coin.current_price}</td>
                                                     :
-                                                    <div className="text-xs font-medium text-yellow-500">{coin.current_price}</div>
-                                                    
-                                                    }
-                                                </div>
+                                                    <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-yellow-500">{coin.current_price}</td>
+                                                }
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-orange-500">
+                                                {coin.atl}
                                             </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-orange-500">{coin.atl}</div>
-                                                </div>
-                                            </td>
-                                            <td className="px-3 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-green-600">{coin.ath}</div>
-                                                </div>
+                                            <td className="px-3 py-3 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                {coin.ath}
                                             </td>
                                             
                                         </tr>
--- frontend/components/list.jsx
+++ frontend/components/list.jsx
@@ -6,7 +6,7 @@ export default function List(props) {
     const coins = Object.values(props.coins)
     if (coins.length > 0) {
         return (
-                            <table className="w-1/3 divide-y divide-gray-200 overflow-y-scroll border-solid border 2 border-grey-500">
+                            <table className="w-1/3 divide-y divide-gray-200 overflow-y-scroll border-solid border 2 border-grey-500 table-auto h-1/2">
                                 <thead className="bg-gray-50">
                                     <tr>
                                         <th scope="col" className="px-6 py-3 text-center text-sm text-gray-500 uppercase ">Id</th>
@@ -15,29 +15,21 @@ export default function List(props) {
                                         <th scope="col" className="px-6 py-3 text-center text-sm text-gray-500 uppercase ">Name</th>
                                     </tr>
                                 </thead>
-                                    <tbody className="bg-white divide-y divide-gray-200 overflow-y-scroll divide-gray-200 ">
+                                    <tbody className="bg-white divide-y divide-gray-200 overflow-y-scroll divide-gray-200 h-inherit">
           
                                     {coins.map(coin => (
-                                        <tr key={coin.id}>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.id}</div>
-                                                    </div>
+                                        <tr key={coin.id} className='w-inherit'>
+                                            <td className="px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                {coin.id}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="flex text-xs font-medium justify-self-center text-gray-900">{coin.symbol}</div>
-                                                </div>
+                                            <td className="px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                               {coin.symbol}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <img className="h-10" src={coin.image}></img>
-                                                </div>
+                                            <td className="px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                <img className="h-10" src={coin.image}/>
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="flex h-inherit justify-center align-center">
-                                                    <div className="text-xs font-medium text-gray-900">{coin.name}</div>
-                                                    </div>
+                                            <td className="px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+                                                {coin.name}
                                             </td>
                                             
                                         </tr>
